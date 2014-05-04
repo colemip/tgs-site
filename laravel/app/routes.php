eb7aeebb3f98f3b0904f9f83d015ac11afee93f3
Ian Coleman <colemip@gmail.com>
2014-05-04 15:43:51 -0400

    Adding routes I anticipate needing

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -15,3 +15,11 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('about', function() {
+	return 'About!';
+});
+
+Route::get('submit', function() {
+	return 'Submit';
+});
