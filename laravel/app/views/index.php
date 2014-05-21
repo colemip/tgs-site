ae11c6042070f5046a0adec905c18b4e8bfc8647
Ian Coleman <colemip@gmail.com>
2014-05-20 23:04:03 -0400

    Setting up nav

---
+++ laravel/app/views/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Triangle Game Showcase | Raleigh, NC</title>
+	<link rel='stylesheet' type='text/css' href='css/common.css'> 
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Submit</li>
+			</ul>
+		</nav>
+	</header>
+	<main></main>
+	<footer></footer>
+</body>
+</html>
