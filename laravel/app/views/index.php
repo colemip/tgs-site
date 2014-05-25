0d5b75be35562429b2518a2c3ea861cbd088e89d
Ian Coleman <colemip@gmail.com>
2014-05-25 00:30:40 -0400

    [#1] Article styling + editing intro text

--- laravel/app/views/index.php
+++ laravel/app/views/index.php
@@ -20,12 +20,14 @@
 	<main>
 	    <article id='tgs-intro'>
 	        <div class='article-wrapper'>
-               Submissions will be accepted starting <?php echo $submitDate; ?>.<br /> Check back here for more info soon!
+                The Triangle Game Showcase is an outlet for creative, innovative game developers in the NC Triangle area to 
+                put their talent on display. Judges will favor titles developed in the Triangle, but all entries will be considered 
+                for the showcase. We can't wait to see what you have been working on!  
             </div>
 	    </article>
 	    <article id='tgs-timeline' class=''>
 	        <div class='article-wrapper'>
-	           Submissions will be accepted starting <?php echo $submitDate; ?>!
+	           Submissions will be accepted starting <?php echo $submitDate; ?>.<br /> Check back here for more info soon!
 	        </div>
 	    </article>
 	</main>
