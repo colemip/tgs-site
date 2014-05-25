619eb022ed4857e43b45f591bd3fa2402254f74d
Ian Coleman <colemip@gmail.com>
2014-05-25 00:35:12 -0400

    [#1] Adjusting bg color for articles + adding new article for judges info

--- laravel/app/views/index.php
+++ laravel/app/views/index.php
@@ -22,12 +22,17 @@
 	        <div class='article-wrapper'>
                 The Triangle Game Showcase is an outlet for creative, innovative game developers in the NC Triangle area to 
                 put their talent on display. Judges will favor titles developed in the Triangle, but all entries will be considered 
-                for the showcase. We can't wait to see what you have been working on!  
+                for the showcase.
             </div>
 	    </article>
 	    <article id='tgs-timeline' class=''>
 	        <div class='article-wrapper'>
 	           Submissions will be accepted starting <?php echo $submitDate; ?>.<br /> Check back here for more info soon!
+	        </div>
+	    </article>
+	    <article id='tgs-judges'>
+	        <div class='article-wrapper'>
+	            Here is more content.
 	        </div>
 	    </article>
 	</main>
