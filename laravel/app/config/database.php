d61cf1241d4aee0e224448ebb794c34fa4677048
Ian Coleman <colemip@gmail.com>
2014-05-25 22:11:19 -0400

    [#5] Changing db connection settings to use local mysql db

--- laravel/app/config/database.php
+++ laravel/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'trigame',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'bl457er',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
