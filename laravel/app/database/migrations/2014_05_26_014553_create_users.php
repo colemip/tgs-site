0a96c5d6ee40422264ce6890172e5e49d912bb3b
Ian Coleman <colemip@gmail.com>
2014-05-25 22:12:23 -0400

    [#5] Users table migration

---
+++ laravel/app/database/migrations/2014_05_26_014553_create_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('users', function($table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');            
+            $table->timestamps();
+        });
+    }
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('users');
+	}
+
+}
+
+?>
