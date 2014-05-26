1c89bce94cfcc51ea6a37a8677db1c7424710227
Ian Coleman <colemip@gmail.com>
2014-05-26 11:25:51 -0400

    [#1] Adding master view

---
+++ laravel/app/views/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Triangle Game Showcase | SPARKcon 2014 | Raleigh, NC</title>        
+    </head>
+    
+</html>
