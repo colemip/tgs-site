87ebeb7aabe07d5d52371d4f474b82c30e3bb040
Ian Coleman <colemip@gmail.com>
2014-05-27 20:48:02 -0400

    [#9] Building master blade

--- laravel/app/views/master.blade.php
+++ laravel/app/views/master.blade.php
@@ -6,5 +6,17 @@
         <title>Triangle Game Showcase | SPARKcon 2014 | Raleigh, NC</title>     
         <link rel="stylesheet" href="{{asset('bootstrap.min.css')}}">   
     </head>
-    
+    <body>
+        <header>
+            @yield('header')
+        </header>
+        
+        <main>
+            @yield('content')
+        </main>
+        
+        <footer class=''>
+            @yield('footer')
+        </footer>
+    </body>                
 </html>
