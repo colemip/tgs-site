70a9c74b3d580a70ba72e92df5d0b59ec3b675a5
Ian Coleman <colemip@gmail.com>
2014-05-27 21:16:23 -0400

    [#9] Changing index view into a blade

---
+++ laravel/app/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section('content')
+    <article id='tgs-intro'>
+        <div class='article-wrapper'>
+            The Triangle Game Showcase is an outlet for creative, innovative game developers in the NC Triangle area to 
+            put their talent on display. Judges will favor titles developed in the Triangle, but all entries will be considered 
+            for the showcase.
+        </div>
+    </article>
+    <article id='tgs-timeline' class=''>
+        <div class='article-wrapper'>
+           Submissions will be accepted starting <?php echo $submitDate; ?>.<br /> Check back here for more info soon!
+        </div>
+    </article>
+    <article id='tgs-judges'>
+        <div class='article-wrapper'>
+            Here is more content.
+        </div>
+    </article>
+@stop
