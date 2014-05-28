c1fb91fdc4a9539afba4d40e385f538083ad0d86
Ian Coleman <colemip@gmail.com>
2014-05-28 19:53:47 -0400

    [#2] Modifying submit page to use master blade

--- laravel/app/views/submit.blade.php
+++ laravel/app/views/submit.blade.php
@@ -1,20 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Triangle Game Showcase | Raleigh, NC</title>
-    <link rel='stylesheet' type='text/css' href='css/common.css'> 
-</head>
-<body>
-    <main>
-        Submit here: <br /><br />
-            
-            
-            {{Form::label('First Name')}} {{Form::text('first_name')}}
-            {{Form::submit("Register", array("class"=>"btn btn-default"))}}
-            {{Form::close()}}
+@extends('master')
+@section('content')
+    Submit here: <br /><br />
         
         
-    </main>
-</body>
-</html>
+    {{Form::label('First Name')}} {{Form::text('first_name')}}
+    {{Form::submit("Register", array("class"=>"btn btn-default"))}}
+    {{Form::close()}}
+        
+        
+@stop
