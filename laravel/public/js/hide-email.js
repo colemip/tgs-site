58fb64e804b2081178c5e1a552f494ea30100694
Ian Coleman <colemip@gmail.com>
2014-06-02 23:56:01 -0400

    Script for hiding email from bots

--- laravel/public/js/hide-email.js
+++ laravel/public/js/hide-email.js
@@ -1,5 +1,15 @@
 
 
 $(function() {
-    console.log("LOADED!");
+    $(document).ready(function() {
+        $('.hide-email').each(function(index, value) {
+            // $(this).on('click', function(event) {
+                // event.preventDefault();
+                // console.log('CLICKED'); 
+            // });
+            console.log($(this));
+            // var emailLink = '<a>' +  + '</a>';
+            $(this).attr('href', "mailto:" + $(this).attr('id') + "@trianglegameshowcase.com"); 
+        }); 
+    });
 });
