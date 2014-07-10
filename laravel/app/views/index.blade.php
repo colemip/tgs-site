b28a210ae631dc11b0df6b371e8df10166cf2cce
Ian Coleman <colemip@gmail.com>
2014-07-10 18:22:49 -0400

    Updating some homepage text and styling

--- laravel/app/views/index.blade.php
+++ laravel/app/views/index.blade.php
@@ -5,15 +5,17 @@
             The Triangle Game Showcase is an outlet for creative, innovative game developers in the NC Triangle area to 
             put their talent on display. Judges will favor titles developed in the Triangle, but all entries will be considered 
             for the showcase.
+            <br /><br />
+            The Triangle Game Showcase will take place at this year's <a href='http://www.sparkcon.com/'>SPARKcon</a> in downtown Raleigh, NC, September 11-14.
         </div>
     </article>
     <article id='tgs-timeline' class=''>
         <div class='article-wrapper'>
            <a  href='https://docs.google.com/forms/d/1ioy59s0k8JAJ67640JHa8Pdysrny0Lnypk7gMTwdoG8/viewform?usp=send_form' target='_blank'>Complete this form</a> 
            to express your interest in submitting to the showcase.
-           <br /><br /> 
+           <!-- <br /><br /> 
            A separate form for submitting your game will be available
-           in the coming weeks.
+           in the coming weeks. -->
            <!-- <br /><br /> -->
            <!-- If you have any other questions, feel free to email <a id='ian' class='hide-email'>Ian</a> or <a id='rogelio' class='hide-email'>Rogelio</a>.  -->
         </div>
